dfb1d64e143d1548bc614357df741df1f829b118
ananthuuu <ananthcjayan12@gmail.com>
2024-11-22 09:52:13 +0530

    fix env setting

--- FileUploadQRCodeGenerator/wsgi.py
+++ FileUploadQRCodeGenerator/wsgi.py
@@ -4,15 +4,12 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 from dotenv import load_dotenv
 
-# Load environment variables using python-dotenv
-load_dotenv()
-env_value = os.getenv("environment")
+
 
 # Check environment variable and set the appropriate Django settings module
-if env_value == "local":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FileUploadQRCodeGenerator.settings")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FileUploadQRCodeGenerator.settings_prod")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FileUploadQRCodeGenerator.settings")
+
 
 # Get the default Django WSGI application
 application = get_wsgi_application()
